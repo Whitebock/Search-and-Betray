012238bfd7344e4c4231240d758e050e7cb5511d
GreenDonkey <devin.mex@gmx.de>
2016-04-14 20:20:00 +0200

    Added files via upload

---
+++ GameManager/GameManager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class GameManager : MonoBehaviour {
+
+	//keep track of all players in network
+	public static Dictionary<string,playerstats> playerlist = new Dictionary<string, playerstats> ();
+
+	public static void RegisterPlayer(string networkid, playerstats player)
+	{
+		//add to playerlist
+		playerlist.Add (networkid, player);
+
+	}
+
+
+	public static void UnRegisterPlayer(string networkid)
+	{
+		//remove player from list
+		playerlist.Remove (networkid);
+
+	}
+
+	//gets playerstats from passed networkid
+	public static playerstats GetPlayerstats(string networkid)
+	{
+
+		return playerlist [networkid];
+
+	}
+
+	public static List<playerstats> GetAllPlayerstats()
+	{
+		List<playerstats> templist = new List<playerstats> ();
+
+		foreach (var item in playerlist) {
+			templist.Add (item.Value); 
+		}
+
+		return templist;
+
+
+	}
+
+
+}
---
+++ InGameGUI/modelmanagergui.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class modelmanagergui : NetworkBehaviour {
+
+	public GameObject scrollviewcontent;
+
+	public GameObject guiprefabmodel;
+
+	public GameObject[] allmodels;
+
+	public GameObject playerprefab;
+
+	void Start()
+	{
+		SetupScrollMenu();
+	}
+
+
+	void SetupScrollMenu()
+	{
+		foreach (var item in allmodels) {
+			GameObject temp = Instantiate(guiprefabmodel,Vector3.zero,Quaternion.identity) as GameObject;
+			temp.GetComponentInChildren<Text>().text = item.transform.name;
+			temp.transform.SetParent(scrollviewcontent.transform);
+		}
+
+	}
+
+
+	[Command]
+	public void CmdSetModel()
+	{
+		
+
+
+	}
+
+
+	[Command]
+	public void CmdSetName()
+	{
+
+
+		
+	}
+
+}
