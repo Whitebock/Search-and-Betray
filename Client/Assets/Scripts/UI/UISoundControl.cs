e8817e1be10158fb4c0712b325fcb6a356e4c832
Philipp Borucki <philippborucki@Philipps-MBP-2.fritz.box>
2016-05-08 16:56:46 +0200

    Adding Main Menu
    + Main Menu Scene
    + MainMenuManagment
    + UISoundControl
    + Functionality to join games (CCC_Client implementation)

---
+++ Client/Assets/Scripts/UI/UISoundControl.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+//IPointerClickHandler, IPointerDownHandler, IPointerUpHandler, IPointerEnterHandler, IPointerExitHandler, ISelectHandler
+
+public class UISoundControl : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler {
+
+	public AudioSource menuFX;
+	public AudioClip hover,click;
+
+
+	public void OnPointerEnter (PointerEventData eventData)
+	{
+		if (hover != null) 
+		{
+			menuFX.clip = hover;
+			menuFX.Play();
+		}
+	}
+
+	public void OnPointerClick(PointerEventData eventData)
+	{
+		if (click != null) 
+		{
+			menuFX.clip = click;
+			menuFX.Play();
+		}
+	}
+}
