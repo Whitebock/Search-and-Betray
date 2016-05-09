399cddf04f72141d3c96cf06609321f03798fbf9
Markus Röse <markus.roese@posteo.de>
2016-05-09 11:58:09 +0200

    Added tracer
    Added tracer

---
+++ Client/Assets/Scripts/Items/Tracer.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+/*Markus Röse
+  Script to visual represent bullets in the scene
+  */
+
+
+public class Tracer : MonoBehaviour {
+   
+    Ray myRay; //Ray which was send
+    RaycastHit myRaycastHit; //Hit point of bullet
+
+    LineRenderer myLineRenderer; //LineRenderer for visual representation in scene
+
+    int distance = 0;
+
+	
+	void Update () {
+		//Leave method if fields are not properly set
+		if(myLineRenderer == null)
+			return;
+		
+        distance+=2;
+		
+		//Set points of linerenderer for visual representation
+        myLineRenderer.SetPosition(0, myRay.GetPoint(distance - 1));
+        myLineRenderer.SetPosition(1, myRay.GetPoint(distance));
+		
+		//When desired point is reached the gameObject destroys itself
+        if (CheckFinish())
+        {
+            Destroy(gameObject);
+        }
+	}
+
+    /// <summary>
+    /// Method set necessary popertys to set flight path of tracer.
+    /// </summary>
+    /// <param name="myRay">Ray which was send when weapon was shot.</param>
+    /// <param name="hit">The hit object of the raycast.</param>
+    public void StartTracer(Ray myRay, RaycastHit hit)
+    {
+        myLineRenderer = gameObject.GetComponent<LineRenderer>();
+        this.myRay = myRay;
+        this.myRaycastHit = hit;
+        
+    }
+
+    /// <summary>
+    /// Method to check if desired point is reached
+    /// </summary>
+    /// <returns>A bool which indicates if desired point is reached.</returns>
+    bool CheckFinish()
+    {
+        if (distance >= myRaycastHit.distance)
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
--- Client/Assets/Scripts/PlayerScripts/FireWeapon.cs
+++ Client/Assets/Scripts/PlayerScripts/FireWeapon.cs
@@ -1,43 +1,53 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System;
-/*
- * Markus Röse: Script zum steuern der Waffe
- */
+/*Markus Röse
+Script zum steuern der Waffe*/
 
-public class FireWeapon : MonoBehaviour
-{
-	private Weapon myWeapon;
-	private AnimatorWeaponPos anim;
-	public Weapon MyWeapon
-	{
-		get { return myWeapon; }
-		set { myWeapon = value; }
+public class FireWeapon : MonoBehaviour {
+    private Weapon myWeapon;
+    public GameObject tracer;
+
+    public Weapon MyWeapon
+    {
+        get
+        {
+            return myWeapon;
+        }
+
+        set
+        {
+            myWeapon = value;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+        FindWeapon();
 	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButton("Fire1"))
+        {
+            //If weapon is actually shot recoil is applied
+            if(MyWeapon.TriggerDown())
+            {
+                CheckHit();
+                // myRecoil.ApplyRecoil(myWeapon.recoilForce); Currently recoil is not working properly
+                ApplyRecoil();
+                
+            }
 
-	void Start()
-	{
-		anim = new AnimatorWeaponPos(GetComponentInChildren<Animator>());
-	}
-
-	void Update()
-	{
-		if (!myWeapon) return;
-
-		if (Input.GetButton ("Fire1"))
-		{
-			if (myWeapon.TriggerDown()) // If weapon is actually shot recoil is applied
-			{
-				CheckHit();
-				anim.Shoot();
-				ApplyRecoil();
-			}
-		}
-		else if (Input.GetButtonUp ("Fire1")) myWeapon.TriggerUp();
-		else if (Input.GetKeyDown (KeyCode.R))
-		{
-			if (myWeapon.Reload()) anim.Reload();
-		}
+        }
+        else if (Input.GetButtonUp("Fire1"))
+        {
+            MyWeapon.TriggerUp();
+        }
+        else if(Input.GetKeyDown(KeyCode.R))
+        {
+            MyWeapon.Reload();
+        }
 	}
 
     private void ApplyRecoil()
@@ -45,18 +55,44 @@ public class FireWeapon : MonoBehaviour
         transform.Rotate(new Vector3(UnityEngine.Random.Range(0.2f, 0.4f) * -1,0,0));
     }
 
-    void CheckHit()
+    void FindWeapon()
     {
-        RaycastHit hit;
-        Ray ray = Camera.main.ScreenPointToRay(new Vector2(Screen.width / 2 + UnityEngine.Random.Range(-myWeapon.recoilForce, myWeapon.recoilForce),
-            Screen.height / 2 + UnityEngine.Random.Range(-myWeapon.recoilForce, myWeapon.recoilForce)));
-		
-        if (Physics.Raycast(ray, out hit))
+        foreach (Transform item in transform)
         {
-			if (hit.transform.gameObject.GetComponent<DamageHandler>())
+            if (item.gameObject.tag == "Weapon")
             {
-                hit.transform.gameObject.GetComponent<DamageHandler>().TakeDamage(myWeapon.damage);
+                if (item.GetComponent<Weapon>())
+                {
+                    if(item.GetComponent<Weapon>().enabled)
+                        MyWeapon = item.GetComponent<Weapon>();
+                }
             }
         }
     }
+    void CheckHit()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(new Vector2(Screen.width / 2 + UnityEngine.Random.Range(-1 * MyWeapon.recoilForce, MyWeapon.recoilForce),
+            Screen.height / 2 + UnityEngine.Random.Range(-1 * MyWeapon.recoilForce, MyWeapon.recoilForce)));
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.transform.gameObject.GetComponent<DamageHandler>())
+            {
+                hit.transform.gameObject.GetComponent<DamageHandler>().TakeDamage(MyWeapon.damage);
+            }
+        }
+        else
+        {
+            hit = new RaycastHit();
+            hit.point =  ray.GetPoint(300);
+            hit.distance = 300;
+            
+        }
+
+        //Setting ray origin to weapon
+        ray.origin = myWeapon.transform.position;
+
+        GameObject t = Instantiate(tracer, gameObject.transform.position, Quaternion.identity) as GameObject;
+        t.GetComponent<Tracer>().StartTracer(ray, hit);
+    } 
 }
