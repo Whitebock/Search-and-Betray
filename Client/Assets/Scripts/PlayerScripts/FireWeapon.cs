c3bd95eb2d0340ca7d9c243118bbf90e7ae8eb0c
Steven Pauls <stevenpauls@gmx.de>
2016-05-14 17:38:25 +0200

    Raycasting-Fehler gefixt. Spieler können mit anderen Spielern collidieren und Waffen können die Hitboxen Treffen ohne den OnlinePlayerCollider vorher zu treffen. Collider der Shootguns lag auf einem falschen Layer. Das ist jetzt auch behoben.

--- Client/Assets/Scripts/PlayerScripts/FireWeapon.cs
+++ Client/Assets/Scripts/PlayerScripts/FireWeapon.cs
@@ -12,11 +12,6 @@ public class FireWeapon : MonoBehaviour
     public Weapon MyWeapon
     { get { return myWeapon; } set { myWeapon = value; } }
 
-    void Start ()
-	{
-        FindWeapon();
-	}
-
 	void Update ()
 	{
 		if (myWeapon == null) return;
@@ -49,48 +44,31 @@ public class FireWeapon : MonoBehaviour
         transform.Rotate(new Vector3(UnityEngine.Random.Range(0.2f, 0.4f) * -1,0,0));
     }
 
-    void FindWeapon()
-    {
-        foreach (Transform item in transform)
-        {
-            if (item.gameObject.tag == "Weapon")
-            {
-                if (item.GetComponent<Weapon>())
-                {
-                    if(item.GetComponent<Weapon>().enabled)
-                        MyWeapon = item.GetComponent<Weapon>();
-                }
-            }
-        }
-    }
-
     void CheckHit()
 	{
         Ray ray = Camera.main.ScreenPointToRay(new Vector2(Screen.width / 2 + UnityEngine.Random.Range(-1 * MyWeapon.recoilForce, MyWeapon.recoilForce),
             Screen.height / 2 + UnityEngine.Random.Range(-1 * MyWeapon.recoilForce, MyWeapon.recoilForce)));
 
 		RaycastHit hit;
-		Physics.Raycast(ray, out hit);
+		Physics.Raycast(ray, out hit, Mathf.Infinity);
 
 		//Setting ray origin to weapon
 		ray.origin = myWeapon.transform.position;
-
 		GameObject t = Instantiate(tracer, gameObject.transform.position, Quaternion.identity) as GameObject;
 		t.GetComponent<Tracer>().StartTracer(ray, hit);
 
-		DamageHandler hitPlayer = null;
+		/* DEBUG
+		if (hit.transform)
+		{
+			if (hit.transform.GetComponent<DamageHandler>()) Debug.Log(hit.transform.name + " | " + LayerMask.LayerToName(hit.transform.gameObject.layer) + " (" + hit.transform.gameObject.layer + ") | DemageHandler attached!");
+			else Debug.Log(hit.transform.name + " | " + LayerMask.LayerToName(hit.transform.gameObject.layer) + " (" + hit.transform.gameObject.layer + ")");
+		}
+		else Debug.Log("Hit nothing"); */
 
+		DamageHandler hitPlayer = null;
 		if (hit.transform != null) hitPlayer = hit.transform.GetComponent<DamageHandler>();
 
-		if (hitPlayer)
-        {
-            Debug.Log("SEND_SHOT_DAMAGE");
-			CCC_Client.Instance.SendShot(hit.point, hitPlayer.AttachedPlayer.PlayerID, MyWeapon.damage);
-        }
-        else
-        {
-            Debug.Log("SEND_SHOT");
-            //CCC_Client.Instance.SendShot(hit.point);
-		}
+		if (hitPlayer) CCC_Client.Instance.SendShot(hit.point, hitPlayer.AttachedPlayer.PlayerID, MyWeapon.damage);
+        else CCC_Client.Instance.SendShot(hit.point);
     }
 }
