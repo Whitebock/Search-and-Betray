7f43278e9a0c50421747425ff3658fae62ebb668
Markus Röse <roese299634@me.com>
2016-05-18 15:34:34 +0200

    Fixed and commented weapons script
    Fixed weapon script:
    -Single fire now works properly
    -Reloading can not be stacked any longer

--- Client/Assets/Scripts/Items/Weapon.cs
+++ Client/Assets/Scripts/Items/Weapon.cs
@@ -24,72 +24,106 @@ public class Weapon : MonoBehaviour
     public uint shotsInMag;					//Current bullet count in mag
     bool readyToShoot;
     bool reloading;
+    bool smallReloading;
 
 	void Start()
 	{
         readyToShoot = true;
-        reloading = false;
+        smallReloading = reloading = false;
         myAudioSource = GetComponent<AudioSource>();
         shotsInMag = shotsPerMag;
 		anim = new AnimatorWeaponPos(GameObject.Find("PlayerWeaponPos").GetComponent<Animator>());
 	}
-	
+
+    /// <summary>
+    /// Emulates trigger down event from real weapon.
+    /// </summary>
     public bool TriggerDown()
 	{
-        if (readyToShoot && shotsInMag > 0 && !reloading)
+        
+        if (readyToShoot && shotsInMag > 0 && !reloading && !smallReloading)
 		{
+            NotReadyToShoot();
             PlaySound(shotSound);
 			anim.Shoot();
             shotsInMag--;
-			NotReadyToShoot();
             return true;
         }
-		else if (readyToShoot && shotsInMag <= 0 && !reloading)
+		else if (readyToShoot && shotsInMag <= 0 && !reloading && !smallReloading)
 		{
-			PlaySound(emptySound);
-			NotReadyToShoot();
+            NotReadyToShoot();
+            PlaySound(emptySound);
 			return false;
 		}
         return false;
     }
 
+    /// <summary>
+    /// Emulates trigger up event from real weapon.
+    /// </summary>
     public void TriggerUp()
     {
-        if (singleShot && !reloading) Invoke("SmallReload", 1.0f / shotsPerSecond);
+        if (singleShot && !smallReloading)
+        {
+            smallReloading = true;
+            Invoke("SmallReload", 1.0f);
+            Debug.Log((1.0f / shotsPerSecond).ToString());
+        }
+       
     }
 
+    /// <summary>
+    /// Reloads weapon
+    /// </summary>
+    /// <returns>Bool which indicates if reload was successfull</returns>
     public bool Reload()
     {
-		if (shotsTotal > 0 && shotsInMag < shotsPerMag)
+        if (!reloading)
         {
-            readyToShoot = false;
-            reloading = true;
-			PlaySound(reloadStartSound);
-			anim.StartReload();
-			Invoke("ReloadAlmostFinished", reloadTime - 0.5f);
-            Invoke("ReloadFinish", reloadTime);
-			return true;
+            if (shotsTotal > 0 && shotsInMag < shotsPerMag)
+            {
+                readyToShoot = false;
+                reloading = true;
+                PlaySound(reloadStartSound);
+                anim.StartReload();
+                Invoke("ReloadAlmostFinished", reloadTime - 0.5f);
+                Invoke("ReloadFinish", reloadTime);
+                return true;
+            }
         }
-		else return false;
-	}
+        return false;
+    }
 
+    /// <summary>
+    /// Disables shoot function on weapon
+    /// </summary>
 	void NotReadyToShoot()
 	{
 		readyToShoot = false;
 		if (!singleShot) Invoke("SmallReload", 1.0f / shotsPerSecond); //Reset weapon to limit shots per second
 	}
 
+    /// <summary>
+    /// Called after a single shot has been fired.
+    /// </summary>
 	void SmallReload()
 	{
 		readyToShoot = true;
-	}
+        smallReloading = false;
+    }
 
+    /// <summary>
+    /// Plays reload sound.
+    /// </summary>
 	void ReloadAlmostFinished()
 	{
 		PlaySound(reloadFinishSound);
 		anim.FinishReload();
 	}
 
+    /// <summary>
+    /// Method which is called when reloading is finished. Refills magazine.
+    /// </summary>
     void ReloadFinish()
     {
         for (shotsInMag = 0; shotsInMag < shotsPerMag && shotsTotal > 0; shotsTotal--) shotsInMag++;
@@ -97,6 +131,10 @@ public class Weapon : MonoBehaviour
         reloading = false;
     }
 
+    /// <summary>
+    /// Method to play sound with weapon as source.
+    /// </summary>
+    /// <param name="toPlay">Audioclip which shall be played</param>
     void PlaySound(AudioClip toPlay)
     {
         if (myAudioSource != null)
