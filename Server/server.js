16dd12839a5c47927e67f4bf09fdc4e6b56be07c
Sven Drewniok <whitebock@web.de>
2016-06-08 21:31:00 +0200

    Logging and Serverconfig
    Better, colorcoded, logging and improved messages. Also, config.json
    will be read it it exists. Otherwise default values will be used.

--- Server/server.js
+++ Server/server.js
@@ -1,15 +1,17 @@
 var net = require('net');
+var fs = require('fs');
 var iputils = require('./iputil');
 var sab = require('./sab');
 
 const PORT = 63001;
-const ENCODING = "utf16le";
+const ENCODING_UNICODE = "utf16le";
 
 var clients = [];
 var maxplayers = 8;
+var timer;
 
 function socket_connect(socket) {
-    console.log('CONNECTED: ' + socket.remoteAddress + ':' + socket.remotePort);
+    //logStatus(socket.remoteAddress + ':' + socket.remotePort + ' connected...');
 }
 
 function socket_data(socket, data) {
@@ -20,12 +22,16 @@ function socket_data(socket, data) {
         case sab.PacketType.HANDSHAKE:
             // Will check if client and server are using
             // the same protocol version.
-            if (recieved.data.readInt8(0) === sab.VERSION)
+            var userversion = recieved.data.readInt8(0);
+            if (userversion === sab.VERSION) {
                 packet = new sab.Packet(sab.PacketType.HANDSHAKE_OK);
+                logStatus('Successful Handshake with ' + socket.remoteAddress);
+            }
             else {
                 var buf = Buffer.allocUnsafe(1);
                 buf.writeInt8(sab.VERSION, 0);
                 packet = new sab.Packet(sab.PacketType.PROTOCOL_NOT_SUPPORTED, buf);
+                logStatus('Failed Handshake with ' + socket.remoteAddress + ' used version ' + userversion, 1);
             }
             socket.write(packet.toBuffer());
             break;
@@ -34,10 +40,11 @@ function socket_data(socket, data) {
             // Clients can request this to prevent unnecessary requests.
             
             var info = "Node Test Server;false;8;";
-            var buf = Buffer.allocUnsafe(Buffer.byteLength(info, ENCODING))
-            buf.write(info, 0, buf.length, ENCODING);
+            var buf = Buffer.allocUnsafe(Buffer.byteLength(info, ENCODING_UNICODE))
+            buf.write(info, 0, buf.length, ENCODING_UNICODE);
             packet = new sab.Packet(sab.PacketType.INFO_RESPONSE, buf);
             socket.write(packet.toBuffer());
+            logStatus(socket.remoteAddress + ' requested info');
             break;
         case sab.PacketType.LOGIN:
             // Will check for:
@@ -52,10 +59,11 @@ function socket_data(socket, data) {
             if (clients.length >= maxplayers) {
                 packet = new sab.Packet(sab.PacketType.GAME_FULL);
                 socket.write(packet.toBuffer());
+                logStatus(socket.remoteAddress + ' failed to login, game full');
                 break;
             }
             
-            var username = recieved.data.toString(ENCODING);
+            var username = recieved.data.toString(ENCODING_UNICODE);
             
             // Check if another player is already using the given name.
             if (!clients.every(function (client) {
@@ -65,6 +73,7 @@ function socket_data(socket, data) {
             })) {
                 packet = new sab.Packet(sab.PacketType.USERNAME_TAKEN);
                 socket.write(packet.toBuffer());
+                logStatus(socket.remoteAddress + ' failed to login, username "' + username + '" already taken');
                 break;
             };
             
@@ -80,6 +89,7 @@ function socket_data(socket, data) {
             })) {
                 packet = new sab.Packet(sab.PacketType.USERNAME_INVALID);
                 socket.write(packet.toBuffer());
+                logStatus(socket.remoteAddress + ' failed to login, username "' + username + '" invalid');
                 break;
             };
             
@@ -94,14 +104,14 @@ function socket_data(socket, data) {
             socket.player = new sab.Player(username, clients.length + 1);
             
             clients.push(socket);
-            console.log(socket.player.username + ' connected...');
+            logStatus(socket.remoteAddress + ' logged in as ' + socket.player.username);
             
             
             // OK and JOIN packet share the same send data
-            var buf = Buffer.allocUnsafe(1 + Buffer.byteLength(socket.player.username, ENCODING))
+            var buf = Buffer.allocUnsafe(1 + Buffer.byteLength(socket.player.username, ENCODING_UNICODE))
             buf.writeInt8(socket.player.id, 0);
-            buf.write(socket.player.username, 1, buf.length, ENCODING);
-
+            buf.write(socket.player.username, 1, buf.length, ENCODING_UNICODE);
+            
             // Send OK packet to player
             packet = new sab.Packet(sab.PacketType.LOGIN_OK, buf);
             socket.write(packet.toBuffer());
@@ -111,7 +121,7 @@ function socket_data(socket, data) {
             clients.forEach(function (client) {
                 client.write(packet.toBuffer());
             });
-
+            
             break;
         case sab.PacketType.PLAYER_MOVE:
             // Will be fired when the player moves, rotates, 
@@ -145,20 +155,20 @@ function socket_data(socket, data) {
 
 function socket_disconnect(socket, data) {
     if (socket.player !== undefined) {
-        console.log(socket.player.username + ' disconnected...');
+        logStatus(socket.player.username + ' disconnected');
         // Remove client from the array
         clients = clients.filter(function (s) {
             return s.player.id !== socket.player.id;
         });
     }
     else {
-        console.log('DISCONNECTED: ' + socket.remoteAddress + ':' + socket.remotePort);
+        //logStatus(socket.remoteAddress + ':' + socket.remotePort + ' disconnected');
     }
 }
 
 function socket_error(socket, data) {
     if (socket.player !== undefined) {
-        console.log(socket.player.username + ' disconnected with error:');
+        logStatus(socket.player.username + ' disconnected with error:', 1);
         console.error(data);
         // Remove client from the array
         clients = clients.filter(function (s) {
@@ -166,33 +176,80 @@ function socket_error(socket, data) {
         });
     }
     else {
-        console.log('DISCONNECTED WITH ERROR: ' + socket.remoteAddress + ':' + socket.remotePort);
+        logStatus(socket.remoteAddress + ':' + socket.remotePort + ' disconnected with error:', 1);
         console.error(data);
     }
 	
 }
 
-function sendPlayerUpdate(player){
+function sendPlayerUpdate(player) {
     var packet = new sab.Packet(sab.PacketType.PLAYER_UPDATE, player.toBuffer());
     clients.forEach(function (client) {
-        if (client.player.id !== player.id) {
+        //if (client.player.id !== player.id) {
             client.write(packet.toBuffer());
-        }
+        //}
     });
 }
 
-console.log('---Search and Betray Server V' + sab.VERSION + '---\r\n');
+function sendSync() {
+    //logStatus('Send sync packet');
+}
+
+function logStatus(message, errorlevel) {
+    if (errorlevel === undefined)
+        errorlevel = 0;
+    var output = '[';
+    switch (errorlevel) {
+        case 0:
+            output += '\033[36mInfo';
+            break;
+        case 1:
+            output += '\033[33mWarning';
+            break;
+        case 2:
+            output += '\033[31mError';
+            break;
+        default:
+    }
+    output += '\033[0m] ' + message;
+    console.log(output);
+}
+
+logStatus('Search and Betray Server, Version ' + sab.VERSION);
 if (process.version !== 'v6.2.1') {
-    console.error('Wrong Node.js Version', '\n\rRequired: v6.2.1', '\r\nUsing: ' + process.version);
+    logStatus('Wrong Node.js Version', 2);
+    logStatus('Required: v6.2.1', 2);
+    logStatus('Using: ' + process.version, 2);
     process.exit()
 }
 
 iputils.getLocalIP(function (err, localip) {
     iputils.getPublicIP(function (err, publicip) {
         
-        console.log('Local IP: ' + localip);
-        console.log('Public IP: ' + publicip);
-        console.log('Port: ' + PORT);
+        var config = {
+            name: 'SAB Server',
+            port: 63001,
+            synctimer: 1000
+        };
+        var configfile = 'config.json';
+        try {
+            var rconf = JSON.parse(fs.readFileSync(configfile));
+            config.name = rconf.name !== undefined ? rconf.name : config.name;
+            config.port = rconf.port !== undefined ? rconf.port : config.port;
+            config.synctimer = rconf.synctimer !== undefined ? rconf.synctimer : config.synctimer;
+            logStatus('Read configuration file');
+        } catch (e) {
+            logStatus('Cannot read ' + configfile + ', using defaults', 1);
+        }
+        
+        logStatus('Name: ' + config.name);
+        if (config.synctimer === 0)
+            logStatus('Sync disabled');
+        else
+            logStatus('Sync enabled: ' + config.synctimer + 'ms');
+        logStatus('Local IP: ' + localip);
+        logStatus('Public IP: ' + publicip);
+        logStatus('Port: ' + PORT);
         
         net.createServer(function (socket) {
             
@@ -209,6 +266,8 @@ iputils.getLocalIP(function (err, localip) {
 			
         }).listen(PORT, localip);
         
-        console.log('Listening...');
+        logStatus('Listening...');
+        if (config.synctimer !== 0)
+            timer = setInterval(sendSync, config.synctimer);
     });
 });
